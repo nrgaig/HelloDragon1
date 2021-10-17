9a7c68d14551712dda5ecf2495e50d27e4b49e0a
maor <frost@g.jct.ac.il>
2021-10-17 19:26:43 +0300

    add_vscode_support

--- exe3p1.cpp
+++ exe3p1.cpp
@@ -1,4 +1,38 @@
-//
-// Created by Maor on 17.10.2021.
+// Created by Maor Frost 206370231
+//  introduction for cpm-sci - Meir Komar
+//	exercise 3 task 4
+//	getting numbers and tells if they can make triangle
 //
 
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    int side1, side2, side3;
+    char char1;
+
+    cout << "enter 3 numbers:" << endl;
+    cin >> side1>>side2>> side3;
+
+    //checking sizes
+    if (side1==side2 && side2==side3)
+    {
+        cout<< "equilateral triangle" <<endl;
+    } else if (side1==side2 || side2==side3 || side1==side3)
+    {
+        cout<< "isosceles triangle" << endl;
+    } else if (side1+side2 > side3 && side2+side3 > side1 && side1+side3 > side2)
+    {
+        cout << "scalene triangle" << endl;
+    } else cout<< "cannot form a triangle" <<endl;
+
+
+
+}
+//Users/maor/CLionProjects/HelloDragon1/cmake-build-debug/Hello1
+//enter 3 numbers:
+//3 4 5
+//scalene triangle
+//
+//Process finished with exit code 0
--- shlomo.cpp
+++ shlomo.cpp
@@ -1,4 +1,28 @@
 //
 // Created by Maor on 12.10.2021.
 //
+#include <cstdio>
+#include<iostream>
+using namespace std;
 
+int main()
+{
+    int b,c;
+    int a=b=c=0;
+    cout << a <<b << c ;
+    int choose1=0;
+    cout <<" if your name is shlomo press 1"<< endl;
+    cin >> choose1;
+
+    if(choose1==1){
+        cout<< "tinok tinok!!\nGO AWAY!!"<< endl;
+        return 0;
+    }
+
+    else{
+        cout<< "gever gever!!"<<endl;
+    }
+
+    cout<< "tnx for using my program :)"<<endl;
+    return 0;
+}
