6955f7349f7e2676b1426805ce0ca8ca5cc09dd1
MaorFrost <maorfrost@gmail.com>
2021-11-13 21:29:34 +0200

    pushed in 13/11/21

---
+++ exe2p3.cpp
@@ -0,0 +1,21 @@
+/*******************************************
+Created by Maor Frost 206370231
+introduction for cpm-sci - Meir Komar
+exercise 6 task 3
+
+********************************************/
+#include <<iostream>>
+
+using namespace std;
+
+int main()
+{
+    int vector[100],num;
+
+    cout<< "enter up to 100  values, to stop enter 0:"<<endl;
+    do
+    {
+        cin>>num;
+    } while (num!=0)
+
+}
